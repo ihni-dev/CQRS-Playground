8594f3d17569caefabf0484ea91538e50ce16a72
Marek <frankowicz.marek@gmail.com>
2019-03-12 22:50:20 +0100

    Extend BoardReadModel

--- CQRS.TaskManagementService/Core/Task Management/CQRS.TaskManagementService.TaskManagement.ReadModels/BoardReadModel.cs
+++ CQRS.TaskManagementService/Core/Task Management/CQRS.TaskManagementService.TaskManagement.ReadModels/BoardReadModel.cs
@@ -5,13 +5,21 @@ using EventFlow.ReadStores;
 
 namespace CQRS.TaskManagementService.TaskManagement.ReadModels
 {
-    public class BoardReadModel : IReadModel, IAmReadModelFor<Board, BoardId, NameSet>
+    public class BoardReadModel : IReadModel, IAmReadModelFor<Board, BoardId, BoardNameChanged>,
+                                                IAmReadModelFor<Board, BoardId, BoardCreated>
     {
-        public string Name { get; private set; }
+        public string Id { get; set; }
+        public string BoardName { get; set; }
 
-        public void Apply(IReadModelContext context, IDomainEvent<Board, BoardId, NameSet> domainEvent)
+        public void Apply(IReadModelContext context, IDomainEvent<Board, BoardId, BoardCreated> domainEvent)
         {
-            Name = domainEvent.AggregateEvent.Name;
+            Id = domainEvent.AggregateIdentity.Value;
+            BoardName = domainEvent.AggregateEvent.BoardName;
+        }
+        
+        public void Apply(IReadModelContext context, IDomainEvent<Board, BoardId, BoardNameChanged> domainEvent)
+        {
+            BoardName = domainEvent.AggregateEvent.BoardName;
         }
     }
 }
